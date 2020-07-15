df93ebc384d98a7a420a7248aaf5523a804b5332
Mike Capecci <mjcapecci@gmail.com>
2020-07-14 22:07:23 -0500

    css fixes

--- src/components/Portfolio/PortfolioController.js
+++ src/components/Portfolio/PortfolioController.js
@@ -26,28 +26,30 @@ const PortfolioController = () => {
   `)
 
   return (
-    <div className="portfolio-container">
-      {allWordpressWpPortfolio.edges.map(card => (
-        <div className="column">
-          <div className="card">
-            <div className="card-image">
-              <img src={card.node.featured_media.source_url} alt="Test" />
+    <div className="portfolio-container-container">
+      <div className="portfolio-container">
+        {allWordpressWpPortfolio.edges.map(card => (
+          <div className="column">
+            <div className="card">
+              <div className="card-image">
+                <img src={card.node.featured_media.source_url} alt="Test" />
+              </div>
+              <div
+                className="card-content"
+                dangerouslySetInnerHTML={{ __html: card.node.excerpt }}
+              ></div>
+              <a
+                href={card.node.acf.portfolio_url}
+                target="_blank"
+                rel="noopener"
+                className="website-link"
+              >
+                Visit Website
+              </a>
             </div>
-            <div
-              className="card-content"
-              dangerouslySetInnerHTML={{ __html: card.node.excerpt }}
-            ></div>
-            <a
-              href={card.node.acf.portfolio_url}
-              target="_blank"
-              rel="noopener"
-              className="website-link"
-            >
-              Visit Website
-            </a>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   )
 }
