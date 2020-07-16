2938cd1544afe2c14a06a8c3c8f34fb53d970638
Mike Capecci <mjcapecci@gmail.com>
2020-07-16 18:58:46 -0500

    attempts to add og:image and twitter image

--- src/components/Default/seo.js
+++ src/components/Default/seo.js
@@ -51,7 +51,7 @@ function SEO({ description, lang, meta, title }) {
         },
         {
           property: `og:image`,
-          content: `https://www.michaelcapecci.com/` + ogImage,
+          content: `https://www.michaelcapecci.com` + ogImage,
         },
         {
           property: `og:type`,
@@ -71,7 +71,7 @@ function SEO({ description, lang, meta, title }) {
         },
         {
           name: `twitter:image`,
-          content: `https://www.michaelcapecci.com/` + ogImage,
+          content: `https://www.michaelcapecci.com` + ogImage,
         },
         {
           name: `twitter:description`,
