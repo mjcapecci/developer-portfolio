1b7160931f66bccae606482a104f0d1c043896b5
Mike Capecci <mjcapecci@gmail.com>
2021-02-27 22:25:34 -0600

    attempt to delay social buttons to hide weird load in

--- src/components/Social/Socials.js
+++ src/components/Social/Socials.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import {
   faTwitterSquare,
@@ -9,74 +9,84 @@ import {
 } from "@fortawesome/free-brands-svg-icons"
 
 const Socials = () => {
+  const [init, setInit] = useState(false)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setInit(true)
+    }, 500)
+  }, [])
+
   return (
-    <div className="social-template">
-      <div className="contact-social-icons">
-        <a
-          className="has-tooltip-bottom"
-          data-tooltip="GitHub"
-          href="https://github.com/mjcapecci"
-          target="_blank"
-          rel="nofollow noopener"
-        >
-          <FontAwesomeIcon
-            icon={faGithubSquare}
-            size="2x"
-            style={{ color: "#fff" }}
-          />
-        </a>
-        <a
-          className="has-tooltip-bottom"
-          data-tooltip="Twitter"
-          href="https://twitter.com/MichaelCapecci"
-          target="_blank"
-          rel="nofollow noopener"
-        >
-          <FontAwesomeIcon
-            icon={faTwitterSquare}
-            size="2x"
-            style={{ color: "#fff" }}
-          />
-        </a>
-        <a
-          className="has-tooltip-bottom"
-          data-tooltip="LinkedIn"
-          href="https://www.linkedin.com/in/michaelcapecci/"
-          target="_blank"
-          rel="nofollow noopener"
-        >
-          <FontAwesomeIcon
-            icon={faLinkedin}
-            size="2x"
-            style={{ color: "#fff" }}
-          />
-        </a>
+    init && (
+      <div className="social-template">
+        <div className="contact-social-icons">
+          <a
+            className="has-tooltip-bottom"
+            data-tooltip="GitHub"
+            href="https://github.com/mjcapecci"
+            target="_blank"
+            rel="nofollow noopener"
+          >
+            <FontAwesomeIcon
+              icon={faGithubSquare}
+              size="2x"
+              style={{ color: "#fff" }}
+            />
+          </a>
+          <a
+            className="has-tooltip-bottom"
+            data-tooltip="Twitter"
+            href="https://twitter.com/MichaelCapecci"
+            target="_blank"
+            rel="nofollow noopener"
+          >
+            <FontAwesomeIcon
+              icon={faTwitterSquare}
+              size="2x"
+              style={{ color: "#fff" }}
+            />
+          </a>
+          <a
+            className="has-tooltip-bottom"
+            data-tooltip="LinkedIn"
+            href="https://www.linkedin.com/in/michaelcapecci/"
+            target="_blank"
+            rel="nofollow noopener"
+          >
+            <FontAwesomeIcon
+              icon={faLinkedin}
+              size="2x"
+              style={{ color: "#fff" }}
+            />
+          </a>
 
-        <a
-          className="has-tooltip-bottom"
-          data-tooltip="Dev.to"
-          href="https://dev.to/mjcapecci"
-          target="_blank"
-          rel="nofollow noopener"
-        >
-          <FontAwesomeIcon icon={faDev} size="2x" style={{ color: "#fff" }} />{" "}
-        </a>
+          <a
+            className="has-tooltip-bottom"
+            data-tooltip="Dev.to"
+            href="https://dev.to/mjcapecci"
+            target="_blank"
+            rel="nofollow noopener"
+          >
+            <FontAwesomeIcon icon={faDev} size="2x" style={{ color: "#fff" }} />{" "}
+          </a>
 
-        <a
-          className="has-tooltip-bottom last-icon"
-          data-tooltip="Stack Overflow"
-          href="https://stackoverflow.com/users/12276598/mjcapecci"
-          target="_blank"
-          rel="nofollow noopener"
-        >
-          <FontAwesomeIcon
-            icon={faStackOverflow}
-            size="2x"
-            style={{ color: "#fff" }}
-          />
-        </a>
+          <a
+            className="has-tooltip-bottom last-icon"
+            data-tooltip="Stack Overflow"
+            href="https://stackoverflow.com/users/12276598/mjcapecci"
+            target="_blank"
+            rel="nofollow noopener"
+          >
+            <FontAwesomeIcon
+              icon={faStackOverflow}
+              size="2x"
+              style={{ color: "#fff" }}
+            />
+          </a>
+        </div>
       </div>
-    </div>
+    )
   )
 }
 
