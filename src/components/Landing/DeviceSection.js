e62fdcf038757846447f52df7961325cb2b9a12f
Mike Capecci <mjcapecci@gmail.com>
2021-10-13 13:37:44 -0500

    updates bio text

--- src/components/Landing/DeviceSection.js
+++ src/components/Landing/DeviceSection.js
@@ -23,20 +23,20 @@ const DeviceSection = () => {
         <p>
           Hello!{" "}
           <strong className="attention-color">
-            I'm a software developer from the Chicago area
+            I'm a software developer from the Chicago area.{" "}
           </strong>
-          . Since mid 2019, I have had a particular focus on building
-          applications with React and Node.js.
+          Since mid 2019, I have had a particular focus on building applications
+          with React and Node.js.
         </p>
       </div>
       <div className="devices-image reverse-flex">
         <p>
+          I am a confident communicator with a knack for breaking down,
+          simplifying, and implementing complex requirements. I am focused on
+          writing{" "}
           <strong className="attention-color">
-            I am ready and able to dig into the details of a new project
+            well-documented and well-tested code.
           </strong>
-          , utilize effective communication with the team, bring ideas to the
-          table based on past problems solved and new understandings, and write
-          clean, well-documented code.
         </p>
         <GrowthImage width={init && width / 2}></GrowthImage>
       </div>
